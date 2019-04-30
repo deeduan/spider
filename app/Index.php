d8564db090c01ed8cde637f60e8d3ddcb17bbfe5
Duan you <dee_phlover@163.com>
2019-04-30 18:45:28 +0800

    1、ok

--- app/Index.php
+++ app/Index.php
@@ -8,7 +8,9 @@ try {
     $url = 'https://www.amazon.com/s?k=baby+sleep+sound+machine&page=2&ref=sr_pg_2';
     $ql = QueryList::get($url);
 
-    $rt = $ql->find('.s-result-item[data-asin=B07FFWGW2L]')->attr('data-index');
+    $rt = $ql->find('.s-result-item[data-asin=B07FFWGW2L]')->attr('*');
+    $text = $ql->find('.s-result-item[data-asin=B07FFWGW2L')->text();
+    var_dump($text);
     var_dump($rt);
 } catch (\Exception $e) {
     var_dump('网络超时\n');
